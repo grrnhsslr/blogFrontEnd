fc9aa7d34eb8a25ff8ccc357069d0067d05985b0
grrnhsslr <hasslergarren@gmail.com>
2024-04-15 15:11:42 -0500

    add navigation components with is loggedin props

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from "react";
+import Navigation from './components/Navigation';
+
 
 export default function App(){
-  // return React.createElement('div', {}, React.createElement('h1', {}, "hellow ordl"), React.createElement('h2', {}, 'by grrnhsslr'));
-  const firstName: string = "grrn";
-  const lastName: string = 'hsslr';
-  const isLoggedIn: boolean = true;
+      // return React.createElement('div', {}, React.createElement('h1', {}, "hellow ordl"), React.createElement('h2', {}, 'by grrnhsslr'));
+    const firstName: string = 'Brian';
+    const lastName: string = 'Stanton';
+    const isLoggedIn: boolean = true;
 
-  const posts: {id:Number, title:String}[] = [
+    const posts: {id:number, title:string}[] = [
         {id: 1, title: 'Happy Monday'},
         {id: 2, title: 'React Rules!'},
         {id: 3, title: 'Spring has Sprung'}
-  ]
-  
-  return (
-      <>
-            <h1>Hello WORld</h1>
-            <h2>By: {firstName} {lastName.toUpperCase()}</h2>
-            <h4>{isLoggedIn ? `Welcome Back ${firstName}${lastName}` : 'Please login or signup'}</h4>
-            {posts.map(t => <h4>{t.title}</h4>)}
-      </>
-  )
+    ]
+
+    return (
+        <>
+            <Navigation isLoggedIn={isLoggedIn}/>
+            <h1>Hello World</h1>
+            <h2>{isLoggedIn ? `Welcome Back ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
+            {posts.map( p => <h4 key={p.id}>{p.title}</h4> )}
+        </>
+    )
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,26 @@
+
+type NavigationProps = {
+    isLoggedIn: boolean
+}
+
+export default function Navigation({ isLoggedIn }:NavigationProps){
+   
+    return (
+        <div>
+            <h5>Kekambas Blog</h5>
+            <ul>
+                {isLoggedIn ? (
+                    <>
+                        <li>Create Post</li>
+                        <li>Sign Out</li>
+                    </>
+                ) : (
+                    <>
+                        <li>Sign Up</li>
+                        <li>Log In</li>
+                    </>
+                ) }
+            </ul>
+        </div>
+    )
+}
