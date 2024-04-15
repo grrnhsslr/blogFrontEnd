3f86c5a8b2eed4fec652621382bcd8127e66e888
grrnhsslr <hasslergarren@gmail.com>
2024-04-15 16:00:01 -0500

    Add styling via our own CSS + React-Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Navigation from './components/Navigation';
+import Container from 'react-bootstrap/Container';
+
+
 
 
 export default function App(){
       // return React.createElement('div', {}, React.createElement('h1', {}, "hellow ordl"), React.createElement('h2', {}, 'by grrnhsslr'));
-    const firstName: string = 'Brian';
-    const lastName: string = 'Stanton';
+    const firstName: string = 'Grrn';
+    const lastName: string = 'hsslr';
     const isLoggedIn: boolean = true;
 
     const posts: {id:number, title:string}[] = [
@@ -16,9 +19,11 @@ export default function App(){
     return (
         <>
             <Navigation isLoggedIn={isLoggedIn}/>
-            <h1>Hello World</h1>
-            <h2>{isLoggedIn ? `Welcome Back ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
-            {posts.map( p => <h4 key={p.id}>{p.title}</h4> )}
+            <Container>
+                <h1>Hello World</h1>
+                <h2>{isLoggedIn ? `Welcome Back ${firstName}${lastName}` : 'Please Log In or Sign Up'}</h2>
+                {posts.map( p => <h4 key={p.id}>{p.title}</h4> )}
+            </Container>
         </>
     )
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,6 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 type NavigationProps = {
     isLoggedIn: boolean
@@ -6,21 +9,26 @@ type NavigationProps = {
 export default function Navigation({ isLoggedIn }:NavigationProps){
    
     return (
-        <div>
-            <h5>Kekambas Blog</h5>
-            <ul>
-                {isLoggedIn ? (
-                    <>
-                        <li>Create Post</li>
-                        <li>Sign Out</li>
-                    </>
-                ) : (
-                    <>
-                        <li>Sign Up</li>
-                        <li>Log In</li>
-                    </>
-                ) }
-            </ul>
-        </div>
+        <Navbar data-bs-theme='dark' bg='dark' expand='lg'>
+            <Container fluid>
+                <Navbar.Brand href='/'>Kekambas Blog</Navbar.Brand>
+                <Navbar.Toggle aria-controls='nav-collapse' />
+                <Navbar.Collapse id='nav-collapse'>
+                    <Nav className='me-auto'>
+                        {isLoggedIn ? (
+                            <>
+                                <Nav.Link href='/'>Create Post</Nav.Link>
+                                <Nav.Link href='/'>Log Out</Nav.Link>
+                            </>
+                        ) : (
+                            <>
+                                <Nav.Link href='/'>Sign Up</Nav.Link>
+                                <Nav.Link href='/'>Log In</Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
